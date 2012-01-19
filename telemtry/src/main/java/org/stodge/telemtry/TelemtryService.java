c56afeb464d4e5da8b76afd6199b4ead7229d527
Salim Fadhley <sal@stodge.org>
2012-01-19 22:05:35 +0000

    Getting a load of eclipse stuff working.

--- telemtry/src/main/java/org/stodge/telemtry/TelemtryService.java
+++ telemtry/src/main/java/org/stodge/telemtry/TelemtryService.java
@@ -3,6 +3,7 @@
  */
 package org.stodge.telemtry;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -11,7 +12,12 @@ import android.os.IBinder;
  * @author sal
  *
  */
-public class TelemtryService extends Service {
+public class TelemtryService extends IntentService {
+
+	public TelemtryService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
 
 	/* (non-Javadoc)
 	 * @see android.app.Service#onBind(android.content.Intent)
@@ -20,6 +26,19 @@ public class TelemtryService extends Service {
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	protected void onHandleIntent( Intent intent ) {
+		long endTime = System.currentTimeMillis() + 5*1000;
+	      while (System.currentTimeMillis() < endTime) {
+	          synchronized (this) {
+	              try {
+	                  wait(endTime - System.currentTimeMillis());
+	              } catch (Exception e) {
+	              }
+	          }
+	      }
 	}
 
 }
