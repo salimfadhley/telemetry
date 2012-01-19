c7d917017e98864d2bc6f2ce9107c82680a1e08e
Salim Fadhley <sal@stodge.org>
2012-01-19 21:34:36 +0000

    Add project data

---
+++ telemtry/src/main/java/org/stodge/telemtry/TelemtryService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.stodge.telemtry;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author sal
+ *
+ */
+public class TelemtryService extends Service {
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
